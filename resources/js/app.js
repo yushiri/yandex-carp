53aa3a54adfc1a41ea9fbde3bf3ff099352277e8
danil <dan4opan4ofaceit@gmail.com>
2023-12-25 21:47:44 +0300

    css, js hotfixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -47,7 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('addButton').addEventListener('click', addHero);
 
-
     function displayHeroes(heroes) {
         let heroesContainer = document.getElementById('heroesContainer');
 
@@ -57,15 +56,14 @@ document.addEventListener('DOMContentLoaded', () => {
             let heroDiv = document.createElement('div');
             heroDiv.classList.add('heroes__card');
             heroDiv.innerHTML = `
-            <div class="card__image" 
-                 data-id="${element.id}" 
+            <div class="card__image"
                  style="background: no-repeat center/cover url('${element.image}')">
-                <button data-trigger="delete" data-id=${element.id}
+                <button data-id=${element.id}
                         class="delete__button">
-                    <svg xmlns="http://www.w3.org/2000/svg" 
-                         width="22" 
-                         height="22" 
-                         viewBox="0 0 72 72" 
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         width="22"
+                         height="22"
+                         viewBox="0 0 72 72"
                          fill="currentColor">
                        <path d="M 19 15 C 17.977 15 16.951875 15.390875 16.171875 16.171875 C 14.609875 17.733875 14.609875 20.266125 16.171875 21.828125 L 30.34375 36 L 16.171875 50.171875 C 14.609875 51.733875 14.609875 54.266125 16.171875 55.828125 C 16.951875 56.608125 17.977 57 19 57 C 20.023 57 21.048125 56.609125 21.828125 55.828125 L 36 41.65625 L 50.171875 55.828125 C 51.731875 57.390125 54.267125 57.390125 55.828125 55.828125 C 57.391125 54.265125 57.391125 51.734875 55.828125 50.171875 L 41.65625 36 L 55.828125 21.828125 C 57.390125 20.266125 57.390125 17.733875 55.828125 16.171875 C 54.268125 14.610875 51.731875 14.609875 50.171875 16.171875 L 36 30.34375 L 21.828125 16.171875 C 21.048125 15.391875 20.023 15 19 15 z">
                        </path>
@@ -79,25 +77,25 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
             </div>`;
 
-
             heroesContainer.appendChild(heroDiv);
+
         });
 
         heroes.forEach((heroObject) => {
-            let deleteButton = document.querySelector(`[data-id="${heroObject.id}"] [data-trigger="delete"]`);
+            let deleteButton = document.querySelector(`[data-id="${heroObject.id}"]`);
 
             deleteButton.onclick = () => {
                 const currentId = deleteButton.getAttribute("data-id");
-
                 const updatedHeroes = heroes.filter((heroObject) => {
                     return heroObject.id !== currentId;
                 });
 
                 displayHeroes(updatedHeroes);
+
                 localStorage.setItem('heroes', JSON.stringify(updatedHeroes));
 
+                location.reload();
             };
-
         });
     }
 
